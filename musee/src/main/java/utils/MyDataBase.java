c52233ac892dd42ceb0e2d9ef402c49ef52e4f0a
Mootaz <mootezjebalia@gmail.com>
2024-03-04 20:12:40 +0100

    bismillah

--- musee/src/main/java/utils/MyDataBase.java
+++ musee/src/main/java/utils/MyDataBase.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class MyDataBase {
-    private final String URL="jdbc:mysql://localhost:3306/arthub";
+    private final String URL="jdbc:mysql://localhost:3306/final";
     private final String USER="root";
     private final String PASS="";
     private Connection connection;
